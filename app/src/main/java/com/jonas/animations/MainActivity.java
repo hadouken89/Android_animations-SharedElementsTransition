fa16efbdd540942df61cf2b5d9d08a7bf6c423d2
jonas_lopez <jonnas.88mig@gmail.com>
2019-08-21 09:57:31 -0300

    add transition of wolf Image in MainActivity

--- app/src/main/java/com/jonas/animations/MainActivity.java
+++ app/src/main/java/com/jonas/animations/MainActivity.java
@@ -1,13 +1,36 @@
 package com.jonas.animations;
 
+import android.content.Intent;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        imageView = findViewById(R.id.image_activity_1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+
+                ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(MainActivity.this, imageView, ViewCompat.getTransitionName(imageView));
+                startActivity(intent, options.toBundle());
+            }
+        });
+
     }
 }
